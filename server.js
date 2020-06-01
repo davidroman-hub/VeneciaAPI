467d824dbda0c20075d4a22a52ce3e3d3ca11fe0
davidroman-hub <jobroman83@gmail.com>
2020-06-01 18:08:55 +0100

    first commit- running server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+// conect mongo DB
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+.then(() => console.log('Base de datos DB conectada'))
+.catch( err => console.log('Conexion con Mongo fallo', err))
+
+// App middlewares
+
+app.use(morgan('dev'))
+app.use(bodyParser.json());
+app.use(cors());
+if((process.env.NODE_ENV = 'development')){
+    app.use(cors({origin:`http://localhost:3000`}))
+}
+
+
+// middlewares
+
+
+// run server
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`API is running on port ${port}`);
+});
